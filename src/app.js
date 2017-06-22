a5cded33cb326cd38ffba54f4d981e73e7b5a9c7
shankarkumarc <shankarkumar.chaudhary@alacriti.com>
2017-06-22 10:49:12 +0530

    track mood

---
+++ src/app.js
@@ -0,0 +1,82 @@
+var express = require('express');
+const request = require('request');
+const Promise = require('bluebird');
+var app = express(); // here I use the express() method, instead of the createServer()
+
+
+
+var server = app.listen(3000, function() {
+  console.log('Listening on port %d', server.address().port);
+});
+
+
+
+function sendToAPiAi(userquery) {
+	return new Promise(
+		function(resolve, reject) {
+		try{
+			var accessToken = "57d640e7bdbd47dfa7712f05e722c8a0";
+			var options = {
+				"url": "https://api.api.ai/v1/query?v=20150910",
+				"json": {
+					"query": userquery,
+					"lang": "eng",
+					"sessionId": "123456789"
+				},
+				"headers": {
+					"Authorization": "Bearer " + accessToken,
+					"Content-Type": "application/json"
+				}
+			};
+			request.post(options, function(err, response, body) {
+				if(err){
+					console.log("error");
+					reject(err)
+				}
+				else{
+					var result={
+						"resolvedQuery" : body.result.resolvedQuery,
+						"action" : body.result.action,
+						"params" : body.result.parameters,
+						"speech" : body.result.fulfillment.speech,
+						"messages" : body.result.fulfillment.messages
+					}
+					console.log(body);
+					resolve(result);
+				}
+			});
+		}
+		catch(e){
+			console.log(e);
+			reject(e);
+		}
+	});
+}
+
+
+
+app.get('/', function(req, res){
+  //res.send('Hello World');
+  res.status(200).json({
+    message: "Hello world!"
+  })
+});
+
+
+app.get('/detectmood', function(req, res){
+  //res.send('Hello World');
+  console.log("detectmood of = "+req.query["sentence"])
+  
+  /*res.status(200).json({
+    
+    result:sendToAPiAi(req.query["sentence"])
+  })*/
+  sendToAPiAi(req.query["sentence"])
+  .then(function(result){
+  	console.log(result);
+  	res.status(200).json({
+    
+    "result":result
+  	})
+  })
+});
